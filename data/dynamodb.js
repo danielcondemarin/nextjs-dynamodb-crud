f3399d9b9ad3b59a3bd0ab9bfd7c9345dc8776c6
danielconde <danielconde9@gmail.com>
2019-09-04 11:46:05 +0100

    require aws-sdk inside func

--- data/dynamodb.js
+++ data/dynamodb.js
@@ -1,43 +1,53 @@
-var AWS = require("aws-sdk");
+const TableName = "Todos";
 
-const options = {
-  convertEmptyValues: true,
-  region: "us-east-1"
+const getDynamoDBClient = () => {
+  const AWS = require("aws-sdk");
+
+  const options = {
+    convertEmptyValues: true,
+    region: "us-west-2"
+  };
+
+  const client = process.env.LOCAL_DYNAMO_DB_ENDPOINT
+    ? new AWS.DynamoDB.DocumentClient({
+        ...options,
+        endpoint: process.env.LOCAL_DYNAMO_DB_ENDPOINT
+      })
+    : new AWS.DynamoDB.DocumentClient(options);
+
+  return client;
 };
-
-const client = process.env.LOCAL_DYNAMO_DB_ENDPOINT
-  ? new AWS.DynamoDB.DocumentClient({
-    ...options,
-    endpoint: process.env.LOCAL_DYNAMO_DB_ENDPOINT
-  })
-  : new AWS.DynamoDB.DocumentClient(options);
-
-const TableName = "Todos"
 
 module.exports = {
   readTodos: async () => {
-    const { Items } = await client.scan({
-      TableName
-    }).promise()
+    const { Items } = await getDynamoDBClient()
+      .scan({
+        TableName
+      })
+      .promise();
 
     return Items;
   },
-  getTodo: async (todoId) => {
-    const { Items } = await client.scan({
-      TableName
-    }).promise()
+  getTodo: async todoId => {
+    const { Items } = await getDynamoDBClient()
+      .scan({
+        TableName
+      })
+      .promise();
 
     const todo = Items.find(todo => todo.todoId == todoId);
 
-    return todo
+    return todo;
   },
-  createTodo: async (todoDescription) => {
-    await client.put({
-      TableName,
-      Item: {
-        todoId: Date.now(),
-        todoDescription
-      }
-    }).promise()
+  createTodo: async todoDescription => {
+    await getDynamoDBClient()
+      .put({
+        TableName,
+        Item: {
+          todoId: Date.now(),
+          todoDescription
+        }
+      })
+      .promise();
   }
-}
+};
--- infrastructure/provisionTable.js
+++ infrastructure/provisionTable.js
@@ -2,7 +2,7 @@ var AWS = require("aws-sdk");
 
 AWS.config.update({
   region: "us-west-2",
-  endpoint: "http://localhost:8000"
+  endpoint: process.env.LOCAL_DYNAMO_DB_ENDPOINT
 });
 
 var dynamodb = new AWS.DynamoDB();
@@ -10,21 +10,25 @@ var dynamodb = new AWS.DynamoDB();
 var params = {
   TableName: "Todos",
   KeySchema: [
-    { AttributeName: "todoId", KeyType: "HASH" },  // Partition key
+    { AttributeName: "todoId", KeyType: "HASH" } // Partition key
   ],
-  AttributeDefinitions: [
-    { AttributeName: "todoId", AttributeType: "N" },
-  ],
+  AttributeDefinitions: [{ AttributeName: "todoId", AttributeType: "N" }],
   ProvisionedThroughput: {
     ReadCapacityUnits: 10,
     WriteCapacityUnits: 10
   }
 };
 
-dynamodb.createTable(params, function (err, data) {
+dynamodb.createTable(params, function(err, data) {
   if (err) {
-    console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    console.error(
+      "Unable to create table. Error JSON:",
+      JSON.stringify(err, null, 2)
+    );
   } else {
-    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    console.log(
+      "Created table. Table description JSON:",
+      JSON.stringify(data, null, 2)
+    );
   }
 });
